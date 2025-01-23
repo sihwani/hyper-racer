2ddfe4e056778b4d93d03cf228fdbcdb2d87db86
sihwani <65937222+sihwani@users.noreply.github.com>
2025-01-23 15:54:25 +0900

    HYP-6 게임 시스템 스크립트 작성
    - CarController (좌우 이동)
    - GameManager (게임 시작 및 각종 설정)
    - MoveButton (버튼을 이용한 Car 조작 스크립트)
    - RoadController (Trigger를 이용한 생성 및 제거)

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,17 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int gas = 100;
+    [SerializeField] private float moveSpeed = 1f;
+    
+    public int Gas
     {
-        
+        get => gas;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
+    {
+        StartCoroutine(GasCoroutine());
+    }
+
+    IEnumerator GasCoroutine()
+    {
+        while (true)
+        {
+            gas -= 10;
+            if (gas <= 0) break;
+            yield return new WaitForSeconds(1f);
+        }
+        // TODO: 게임 종료
+    }
+    
+    /// <summary>
+    /// 자동차 이동 메서드
+    /// </summary>
+    /// <param name="direction"></param>
+    public void Move(float direction)
+    {
+        transform.Translate(Vector3.right * direction * moveSpeed * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.55f, 1.55f), 0, transform.position.z);
+    }
+
+    /// <summary>
+    /// 가스 아이템 획득시 호출되는 메서드
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Gas"))
+        {
+            gas += 30;
+            
+            // TODO: 가스아이템 제거
+        }
+    }
+
+    private void Update()
     {
         
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,145 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject carPrefab;
+    [SerializeField] private GameObject roadPrefab;
+    
+    // UI 관련 코드
+    [SerializeField] private MoveButton leftMoveButton;
+    [SerializeField] private MoveButton rightMoveButton;
+    [SerializeField] private TMP_Text gasText;
+
+    // 자동차 
+    private CarController _carController;
+    
+    // 도로 오브젝트 풀
+    private Queue<GameObject> _roadPool = new Queue<GameObject>();
+    private int _roadPoolSize = 3;
+    
+    // 도로 이동
+    private List<GameObject> _activeRoads = new List<GameObject>();
+    
+    // 상태
+    public enum State { Start, Play, End}
+
+    public State GameState { get; private set; } = State.Start;
+    
+    // 싱글톤
+    private static GameManager _instance;
+
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
     }
+    void Start()
+    {
+        // 도로, 자동차 생성
+        // 게임 시작
+        InitializeRoadPool();
+        
+        StartGame();
+
+    }
+
+    private void Update()
+    {
+        foreach (var activeRoad in _activeRoads)
+        {
+            activeRoad.transform.Translate(-Vector3.forward * Time.deltaTime);
+        }
+        
+        // Gas 정도 출력
+        if (_carController != null)
+        {
+            gasText.text = _carController.Gas.ToString();
+        }
+    }
+
+    void StartGame()
+    {
+        // 도로 생성
+        SpawnRoad(Vector3.zero);
+        //자동차 생성
+        _carController = Instantiate(carPrefab, new Vector3(0, 0, -3f), Quaternion.identity).GetComponent<CarController>();
+        
+        // Left, Right move button에 자동차 컨트롤 기능 적용
+        leftMoveButton.OnMoveButtonDown += () => _carController.Move(-1f);
+        rightMoveButton.OnMoveButtonDown += () => _carController.Move(1f);
+    }
+
+    // 도로 생성 및 관리
+
+    #region 도로 생성 및 관리
+
+    /// <summary>
+    /// 도로 오브젝트 풀 초기화
+    /// </summary>
+    private void InitializeRoadPool()
+    {
+        for (int i = 0; i < _roadPoolSize; i++)
+        {
+            GameObject road = Instantiate(roadPrefab);
+            road.SetActive(false);
+            _roadPool.Enqueue(road);
+        }
+    }
+    
+    /// <summary>
+    /// 도로 오브젝트 풀에서 불러와 배치하는 함수
+    /// </summary>
+    public void SpawnRoad(Vector3 position)
+    {
+        if (_roadPool.Count > 0)
+        {
+            GameObject road = _roadPool.Dequeue();
+            road.transform.position = position;
+            road.SetActive(true);
+            
+            // 활성화 된 길을 움직이기 위해 List에 저장
+            _activeRoads.Add(road);
+            
+        }
+        else
+        {
+            GameObject road = Instantiate(roadPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+
+    public void DestroyRoad(GameObject road)
+    {
+        road.SetActive(false);
+        _activeRoads.Remove(road);
+        _roadPool.Enqueue(road);
+    }
+    
+    #endregion
+    
+    
+    
 }
